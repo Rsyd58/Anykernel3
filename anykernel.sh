13b28223a4588ed66867eddd8a6b63d8d83b83a1
Rsyd <rasyid2007alkautsar@gmail.com>
2022-10-19 16:02:58 +0700

    4.9

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Enkanomiya Kernel for Redmi Note 4(x) mido by Rsyd
+kernel.string=Enkanomiya Kernel v4.9 for Redmi Note 4(x) mido by Rsyd
 do.devicecheck=1
 do.modules=0
 do.systemless=0
